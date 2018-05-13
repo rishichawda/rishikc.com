12a23c50e4572e2addf7d6260e5b3df8f30a9072
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-13 22:45:46 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function slide_in_myproj() { 
+    $('body').addClass('geek-background');
+    $('#home').addClass('zoomOut').one("webkitAnimationEnd oAnimationEnd msAnimationEnd animationEnd",
+    function(event) {
+        $('#showcase').addClass('slideInRight');
+        $('#home').removeClass('zoomOut');
+        $('#showcase').show().one("webkitAnimationEnd oAnimationEnd msAnimationEnd animationEnd",
+        function(event) {
+            $('body').removeClass('geek-background')
+            $('#showcase').removeClass('slideInRight');
+        });
+        $('#home').hide();
+});
+ }
+function show_home() { 
+    $('body').addClass('geek-background');
+    $('#showcase').addClass('slideOutRight').one("webkitAnimationEnd oAnimationEnd msAnimationEnd animationEnd",
+    function(event) {
+        $('#home').addClass('zoomIn');
+        $('#showcase').removeClass('slideOutRight');
+        $('#greeter_message').removeClass('jackInTheBox');
+        $('#home').show().one("webkitAnimationEnd oAnimationEnd msAnimationEnd animationEnd",
+        function(event) {
+            $('body').removeClass('geek-background')
+            $('#home').removeClass('zoomIn');
+        });
+        $('#showcase').hide();
+});
+ }
