ae984b576840344ecea8c4b4633c15e4d2d956db
Rishi Kumar <chawda.rishii@gmail.com>
2018-12-08 15:38:36 +0530

    feat(navbar): update navbar styles

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,7 +5,13 @@ class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-                navbar
+                <ul className="navbar-links">
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                    <li>Link 4</li>
+                    <li>Link 5</li>
+                </ul>
             </div>
         );
     }
