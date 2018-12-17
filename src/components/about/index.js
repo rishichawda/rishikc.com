52d9ce3227d68d3898f98e0f39e889beba7d0c30
Rishi Kumar <chawda.rishii@gmail.com>
2018-12-18 01:40:20 +0530

    docs(src): update career section content

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -10,10 +10,10 @@ const About = () => (
     <p>{'And tell you my \'secret mantra\'!'}</p>
     <q>Eat. Sleep. Code. Repeat.</q>
     <p>
-      {'Haha! Expected something else? Well, that\'s where most of my time goes.'}
-      {'Infront of my laptop. If not coding, then either reading documentations,'}
-      {'articles or blogs. If not that, then I\'m just spidering the web, exploring'}
-      {'repositories / codebases, looking at what other programmers and developers'}
+      {'Haha! Expected something else? Well, that\'s where most of my time goes. '}
+      {'Infront of my laptop. If not coding, then either reading documentations, '}
+      {'articles or blogs. If not that, then I\'m just spidering the web, exploring '}
+      {'repositories / codebases, looking at what other programmers and developers '}
       {'are up to.'}
       <span>
         PS: If you have any suggestions, you can ping the details on any of my
@@ -23,7 +23,7 @@ const About = () => (
     <p>
       I also like books, traveling
       <span>
-        {'( yes, infact programmers need it the most - you can\'t keep being'}
+        {'( yes, infact programmers need it the most - you can\'t keep being '}
         {'frustrated 24x7 with your own code! )'}
       </span>
       , good food and music, and art.
