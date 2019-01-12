6096f8eee51466ef1ca09559619d91a3ae33b6a3
rishichawda <chawda.rishii@gmail.com>
2019-01-12 23:11:02 +0530

    fix(src): minor changes

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './index.scss';
-import SkillsCard from './card';
-import reactLogo from '../../assets/skills/react.png';
-import jsLogo from '../../assets/skills/javascript.png';
-import pythonLogo from '../../assets/skills/python.png';
 
 const Skills = () => (
   <section className="container skills-section" id="skills">
     <h2>Skills</h2>
-    <div className="skill-set">
-      <SkillsCard image={jsLogo} className="js-logo" />
-      <SkillsCard image={pythonLogo} />
-      <SkillsCard image={reactLogo} />
-      <SkillsCard image={reactLogo} />
-    </div>
+    <p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
   </section>
 );
 
