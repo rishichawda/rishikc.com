03df961980d27414386f7cca98aa7485e71f6e39
rishichawda <chawda.rishii@gmail.com>
2019-01-24 23:33:22 +0530

    chore(src): remove old code

--- index.js
+++ index.js
@@ -3,42 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
 import './src/index.scss';
-import Navbar from './src/components/navbar/index.js';
 
-const Home = Loadable({
-  loader: () => import('./src/routes/home/index.js'),
-  loading: () => null,
-});
+// const Home = Loadable({
+//   loader: () => import('./src/routes/home/index.js'),
+//   loading: () => null,
+// });
 
-const About = Loadable({
-  loader: () => import('./src/components/about/index.js'),
-  loading: () => null,
-});
+// const About = Loadable({
+//   loader: () => import('./src/components/about/index.js'),
+//   loading: () => null,
+// });
 
-const Skills = Loadable({
-  loader: () => import('./src/components/skills/index.js'),
-  loading: () => null,
-});
 
-const Work = Loadable({
-  loader: () => import('./src/components/work/index.js'),
-  loading: () => null,
-});
-
-const Projects = Loadable({
-  loader: () => import('./src/components/projects/index.js'),
-  loading: () => null,
-});
 
 const App = () => (
-  <React.Fragment>
-    <Navbar />
-    <Home />
-    <About />
-    <Work />
-    <Skills />
-    <Projects />
-  </React.Fragment>
+  <div>clean start</div>
 );
 
 const render = () => {
