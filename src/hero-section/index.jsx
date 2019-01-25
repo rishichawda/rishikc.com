b3d16619c216ec865dda79a20a4bddc078e249b4
rishichawda <chawda.rishii@gmail.com>
2019-01-25 12:53:03 +0530

    feat(intro-card): update intro card

--- src/hero-section/index.jsx
+++ src/hero-section/index.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import {
+  FaLinkedin, FaGithub, FaMedium, FaInstagram,
+} from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
 import heroImage from '../assets/1456505540091_2.jpg'
 import './index.scss'
 
@@ -7,12 +11,45 @@ export default class HeroSection extends Component {
     return (
       <div className="hero-section">
         <div className="container intro-card">
-        <img src={heroImage} alt="hero-image" className="hero-image"/>
-        <div className="hero-intro">
-          <h5>Hello, I am</h5>
-          <h2>Rishi Kumar Chawda</h2>
-          <h4>Web and Mobile Apps developer.</h4>
-        </div>
+          <img src={heroImage} alt="hero-image" className="hero-image" />
+          <div className="hero-intro">
+            <h5>Hello, I am</h5>
+            <h2>Rishi Kumar Chawda</h2>
+            <h4>Web and Mobile App developer.</h4>
+            <p>A programmer with a passion for product design and development who loves working on freelancing and open source projects.</p>
+            <ul>
+              <a href="https://www.linkedin.com/in/rkrishi">
+                <li>
+                  <FaLinkedin />
+                  LinkedIn
+                </li>
+              </a>
+              <a href="https://github.com/rishichawda">
+                <li>
+                  <FaGithub />
+                  Github
+                </li>
+              </a>
+              <a href="https://medium.com/@rishii.kumar.chawda">
+                <li>
+                  <FaMedium />
+                  Medium Blogs
+                </li>
+              </a>
+              <a href="https://www.instagram.com/rishi.py">
+                <li>
+                  <FaInstagram />
+                  Instagram
+                </li>
+              </a>
+              <a href="/">
+                <li>
+                  <IoMdMail />
+                  Mail
+                </li>
+              </a>
+            </ul>
+          </div>
         </div>
       </div>
     )
