b4f211dbc0a72810b831e0cd6dee24b9f5dee8d9
rishichawda <chawda.rishii@gmail.com>
2019-01-25 16:29:05 +0530

    feat(src): add about section

--- src/about/index.js
+++ src/about/index.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './index.scss';
+import developerAtDesk from '../assets/about-right.png';
 
-export default class About extends Component {
-  render() {
-    return (
-      <div className="container about-section">
-        
+export default () => (
+  <section className="container about-section">
+    <div>
+      <div className="about-content">
+        <h4>About me</h4>
+        <p>{'I\'m a web and native apps developer from Bangalore, India.'}</p>
+        <p>I design and develop websites, progressive web applications and native mobile applications for Android and iOS.</p>
+        <p>Apart from good food, traveling, music and art - I am interested in search engine optimisation, analytics, online marketing and data science.</p>
       </div>
+      <div className="about-image">
+        <img src={developerAtDesk} alt="about-right" />
+      </div>
+    </div>
+  </section>
     )
-  }
-}
