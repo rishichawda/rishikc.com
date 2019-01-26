65193c46564b7aba59fd9033100efd791b093a2a
rishichawda <chawda.rishii@gmail.com>
2019-01-26 12:40:03 +0530

    feat(src): add blogs section

---
+++ src/components/blogs/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.scss';
+
+export default () => (
+  <section className="container project-section">
+  <h2>blogs</h2>
+  <div className="inner-container">
+  <div className="card">
+
+    </div>
+    <div className="card">
+
+    </div>
+    <div className="card">
+
+    </div>
+  </div>
+  </section>
+    )
