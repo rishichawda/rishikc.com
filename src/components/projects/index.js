0b857dce0f7f08c8fac40ffc309ccb5ee0808aaf
rishichawda <chawda.rishii@gmail.com>
2019-01-26 18:50:44 +0530

    feat(projects): add badges to recent projects card

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -14,6 +14,11 @@ export default () => (
               <h4>markdown-magic-build-badge</h4>
             </a>
           </div>
+          <div className="badges">
+          <a href="https://www.npmjs.com/package/markdown-magic-branch-badge">
+            <img alt="version-badge" src="https://img.shields.io/npm/v/markdown-magic-branch-badge/latest.svg?style=flat-square" />
+          </a>
+          </div>
           <div className="repo-description">
           <p>
           🎊 A plugin to update your branch badges to point to correct branch status.
@@ -28,6 +33,11 @@ export default () => (
               <h4>react-lite-components</h4>
             </a>
           </div>
+          <div className="badges">
+          <a href="https://www.npmjs.com/package/react-lite-components">
+            <img alt="version-badge" src="https://img.shields.io/npm/v/react-lite-components/latest.svg?style=flat-square" />
+          </a>
+          </div>
           <div className="repo-description">
           <p>
           A React UI library focused on minimum bundle size, simplicity, consistency and responsive design. ✨
@@ -41,6 +51,11 @@ export default () => (
             <a>
               <h4>bgcreate</h4>
             </a>
+          </div>
+          <div className="badges">
+          <a href="https://bgcreate.netlify.com">
+          <img alt="version-badge" src="https://img.shields.io/website-up-down-green-red/https/shields.io.svg?label=click here to visit&style=flat-square" />
+          </a>
           </div>
           <div className="repo-description">
           <p>
