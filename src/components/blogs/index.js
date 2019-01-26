03bf7c7bad4200c54cb9cf11a39906a9efa4893d
rishichawda <chawda.rishii@gmail.com>
2019-01-26 13:06:46 +0530

    fix(blogs-section): add responsiveness to blog section

--- src/components/blogs/index.js
+++ src/components/blogs/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import './index.scss';
 
 export default () => (
   <section className="container blog-section">
   <h2>blogs</h2>
   <div className="inner-container">
+  <Link to="/blogs">
   <div className="card">
 
     </div>
+    </Link>
+    <Link to="/blogs">
     <div className="card">
 
     </div>
+    </Link>
+    <Link to="/blogs">
     <div className="card">
 
     </div>
+    </Link>
   </div>
   </section>
     )
