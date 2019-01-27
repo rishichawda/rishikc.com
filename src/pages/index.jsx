884063b2c28bb797736d98bfdcdafc15eaf7bcc7
rishichawda <chawda.rishii@gmail.com>
2019-01-27 22:55:37 +0530

    perf(hero-section): optimize image loading on hero-section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,19 +7,35 @@ import Blogs from '../components/blogs'
 
 import './index.scss';
 
-const App = () => (
-  <>
-    <Helmet>
-      <html lang="en"></html>
-      <title>{'Rishi Kumar Chawda - Developer, Freelancer | Web and Native Mobile Apps development'}</title>
-      <meta name="description" content="Bangalore, (also, Bengaluru) India based developer. Experienced with web apps, progressive web apps, native apps for Android and iOS. Loves working on freelancing and open source projects." />
-      <meta name="keywords" content="web, progressive, apps, pwa, native, mobile, android, ios, freelance, freelancing, developer, design, development, freelancer, open, source, projects, blog, bangalore, bengaluru, india" />
-    </Helmet>
-    <Hero />
-    <About />
-    <Blogs />
-    <Projects />
-  </>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Helmet>
+          <html lang="en"></html>
+          <title>{'Rishi Kumar Chawda - Developer, Freelancer | Web and Native Mobile Apps development'}</title>
+          <meta name="description" content="Bangalore, (also, Bengaluru) India based developer. Experienced with web apps, progressive web apps, native apps for Android and iOS. Loves working on freelancing and open source projects." />
+          <meta name="keywords" content="web, progressive, apps, pwa, native, mobile, android, ios, freelance, freelancing, developer, design, development, freelancer, open, source, projects, blog, bangalore, bengaluru, india" />
+        </Helmet>
+        <Hero heroImage={this.props.data.heroImage.childImageSharp}/>
+        <About />
+        <Blogs />
+        <Projects />
+      </>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+   query {
+      heroImage: file(relativePath: {eq: "1456505540091_2.png"}) {
+        childImageSharp {
+        fluid(maxWidth: 1240 ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default App
