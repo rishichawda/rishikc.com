bb29fd5a24d0e5e98ec203e5af0b0a83164720af
rishichawda <chawda.rishii@gmail.com>
2019-04-10 23:29:28 +0530

    build(src): use typefaces by kyle

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,8 @@
 // You can delete this file if you're not using it
 
 // Detect service worker updates and reload page.
-require('./src/globals/fonts.scss')
+require('typeface-pt-sans')
+require('typeface-roboto')
+require('typeface-poppins')
 
 exports.onServiceWorkerUpdateFound = () => window.location.reload(true);
