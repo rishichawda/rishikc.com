6a4b4e68ed25680d3391ac40a1c42b1aecd6b974
rishichawda <chawda.rishii@gmail.com>
2021-03-14 15:40:59 +0530

    refactor(src): cleanup and create tsconfig

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hello</h1>
+  </Layout>
+)
+
+export default IndexPage
