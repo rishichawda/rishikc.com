190f0bc2695573569a22b637769473bbb9542548
rishichawda <rishichawda@users.noreply.github.com>
2022-04-23 14:13:50 +0530

    configure plugins
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,8 @@ module.exports = {
     },
     'gatsby-plugin-dark-mode',
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-force-trailing-slashes`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -34,11 +36,42 @@ module.exports = {
       options: {
         gatsbyRemarkPlugins: [
           {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              username: "rishichawda",
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
               inlineCodeMarker: null,
               noInlineHighlight: true,
+            },
+          },
+          `gatsby-remark-copy-linked-files`, // handle gif images
+          {
+            resolve: `gatsby-remark-smartypants`,
+            options: {
+              dashes: "oldschool",
+            },
+          },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
             },
           },
         ],
@@ -65,6 +98,26 @@ module.exports = {
           include: /assets/
         }
       }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7fffd4`,
+        showSpinner: true,
+        minimum: 0.2,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        pluginConfig: {
+          head: true,
+        },
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
