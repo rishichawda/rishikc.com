c64a26c282374b2e83b9f567fba400f857632683
rishichawda <rishichawda@users.noreply.github.com>
2022-04-23 16:50:24 +0530

    list repos, add external links, minor fixes
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-5 w-full text-center bg-brand text-white">Copyright © 2022 Rishi Kumar Chawda. All Rights Reserved.</footer>
+  )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import SEO from "components/seo"
 import GatsbyLink from 'gatsby-link'
 import './index.css'
 import HandsomeGuySVG from "assets/handsome-guy.svg"
-import LinkModal from "components/links"
 import { graphql } from "gatsby"
+import RepoCard from "components/repoCard"
 
 const IndexPage = ({ data }) => (
   <>
@@ -18,7 +18,7 @@ const IndexPage = ({ data }) => (
           <div className="flex flex-col w-8/12 justify-around">
             <h1 className="text-right italic antialiased">Hi, I'm Rishi.</h1>
             {/* <p className="text-right italic font-light antialiased">..and that above is my desk setup when I designed this page</p> */}
-            <p className="font-normal leading-7 tracking-wide antialiased text-base">Currently, I am a software engineer by profession and building
+            <p className="font-normal leading-7 tracking-wide antialiased text-lg">Currently, I am a software engineer by profession and building
               awesome tools with the Chef team at Progress.
 
               Apart from my work, I spend time reading books, painting or
@@ -69,14 +69,16 @@ const IndexPage = ({ data }) => (
           <h2 className="italic antialiased">Open source</h2>
           <div>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-              <div className="px-4 py-6 sm:px-0">
-                <div className="flex flex-row items-center justify-center border-4 border-dashed bg-slate-400 border-gray-500 bg-opacity-20 border-opacity-40 opacity-40 rounded-lg h-96">
-                  <p>Coming soon..</p>
-                </div>
+              <div className="px-4 py-6 sm:px-0 grid grid-cols-3 grid-flow-row gap-4 auto-cols-min gap-y-5">
+                {data.github.viewer.repositories.edges.map((repository, i) => (
+                  <div className="flex items-stretch" key={repository.node.name}>
+                    <RepoCard repository={repository.node} />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-          <a className="text-brand text-xl italic antialiased">See more projects on GitHub</a>
+          <a href="https://github.com/rishichawda" target="_blank" className="text-brand text-xl italic antialiased">See more projects on GitHub</a>
         </div>
         <div className="flex flex-col max-w-7xl mx-auto w-2/3 mb-28 justify-between">
           <div className="flex flex-row justify-between">
@@ -105,8 +107,40 @@ const IndexPage = ({ data }) => (
 )
 
 export const query = graphql`
-query LatestArticles {
-  allMdx(limit: 2, sort: {fields: frontmatter___date, order: DESC}) {
+query HomePageData {
+    github {
+    viewer {
+      repositories(
+        orderBy: {field: STARGAZERS, direction: DESC}
+        first: 6
+        ownerAffiliations: OWNER
+      ) {
+        edges {
+          node {
+            id
+            name
+            url
+            descriptionHTML
+            stargazerCount
+            owner {
+              login
+            }
+            languages(orderBy: {field: SIZE, direction: DESC}, first: 10) {
+              edges {
+                node {
+                  color
+                  name
+                }
+                size
+              }
+              totalSize
+            }
+          }
+        }
+      }
+    }
+  }
+  allMdx: allMdx(limit: 2, sort: {fields: frontmatter___date, order: DESC}) {
     nodes {
       slug
       id
