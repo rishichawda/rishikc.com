63e83f56c9ae2200e76339ed424a5ea06644973b
rishichawda <rishichawda@users.noreply.github.com>
2022-04-23 16:50:24 +0530

    list repos, add external links, minor fixes
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -15,29 +15,43 @@ type HeaderProps = {
   siteTitle: string
 }
 
-const Header = ({ siteTitle }: HeaderProps) => (
-  <header className="absolute flex bg-sky-400 dark:bg-slate-900 2xl:h-28 h-20 w-full">
-    <div className="flex flex-row items-center justify-between xl:max-w-4xl 2xl:max-w-7xl w-full h-full mx-auto px-2">
-      <GatsbyLink to="/" className="p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="82" viewBox="0 0 120 82">
-          <text id="Rishi" transform="translate(0 62)" fill="#fdfdfd" font-size="65" font-family="Caveat-SemiBold, Caveat" font-weight="600"><tspan x="0" y="0">Rishi</tspan></text>
-        </svg>
-      </GatsbyLink>
-      <div className="flex flex-row items-center justify-between">
-        <div className="hidden md:flex flex-row">
-          <LinkedInIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <GitHubIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <TwitterIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <DevIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <DribbbleIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <SOIcon className="w-full h-5 2xl:h-7 ml-5" />
-          <MediumIcon className="w-full h-5 2xl:h-7 ml-5" />
+const Links = [
+  { IconComponent: LinkedInIcon, url: 'https://www.linkedin.com/in/rkrishi' },
+  { IconComponent: GitHubIcon, url: 'https://www.github.com/rishichawda' },
+  { IconComponent: TwitterIcon, url: 'https://twitter.com/rishiikc' },
+  { IconComponent: DevIcon, url: 'https://dev.to/rishikc' },
+  { IconComponent: DribbbleIcon, url: 'https://www.dribbble.com/rishikc' },
+  // { IconComponent: SOIcon, url: 'https://www.linkedin.com/in/rkrishi' },
+  { IconComponent: MediumIcon, url: 'https://medium.com/@rishii.kumar.chawda' },
+]
+
+const Header = ({ siteTitle }: HeaderProps) => {
+  const onClickProfileUrl = url => {
+    if (typeof window !== 'undefined' && url) {
+      if (window.gtag) {
+        window.gtag('event', url, { url })
+      }
+      window.open(url, '_newtab')
+    }
+  }
+  return (
+    <header className="absolute flex bg-sky-400 dark:bg-slate-900 2xl:h-28 h-20 w-full">
+      <div className="flex flex-row items-center justify-between xl:max-w-4xl 2xl:max-w-7xl w-full h-full mx-auto px-2">
+        <GatsbyLink to="/" className="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="120" height="82" viewBox="0 0 120 82">
+            <text id="Rishi" transform="translate(0 62)" fill="#fdfdfd" font-size="65" font-family="Caveat-SemiBold, Caveat" font-weight="600"><tspan x="0" y="0">Rishi</tspan></text>
+          </svg>
+        </GatsbyLink>
+        <div className="flex flex-row items-center justify-between">
+          <div className="hidden md:flex flex-row">
+            {Links.map((item) => <item.IconComponent className="w-full h-5 2xl:h-7 ml-5" onClick={() => onClickProfileUrl(item.url)} />)}
+          </div>
+          <ThemeToggleButton />
         </div>
-        <ThemeToggleButton />
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
