3fdf10df3757a232d4f0d35e0aeb47692217a46b
rishichawda <rishichawda@users.noreply.github.com>
2022-04-24 16:54:02 +0530

    fix logo
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,6 +7,7 @@ import GitHubIcon from 'assets/icon-github.svg'
 import LinkedInIcon from 'assets/icon-linkedin.svg'
 import MediumIcon from 'assets/icon-medium.svg'
 import TwitterIcon from 'assets/icon-twitter.svg'
+import Logo from 'assets/logo.svg'
 import SOIcon from 'assets/icon-stack-overflow.svg'
 import ThemeToggleButton from "./toggle"
 import GatsbyLink from "gatsby-link"
@@ -38,9 +39,10 @@ const Header = ({ siteTitle }: HeaderProps) => {
     <header className="absolute flex bg-sky-400 dark:bg-slate-900 2xl:h-28 h-20 w-full">
       <div className="flex flex-row items-center justify-between xl:max-w-4xl 2xl:max-w-7xl w-full h-full mx-auto px-2">
         <GatsbyLink to="/" className="p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="120" height="82" viewBox="0 0 120 82">
-            <text id="Rishi" transform="translate(0 62)" fill="#fdfdfd" font-size="65" font-family="Caveat-SemiBold, Caveat" font-weight="600"><tspan x="0" y="0">Rishi</tspan></text>
-          </svg>
+          <Logo className="max-h-full w-auto" />
+          {/* <svg xmlns="http://www.w3.org/2000/svg" width="120" height="82" viewBox="0 0 120 82"> */}
+            {/* <text id="Rishi" transform="translate(0 62)" fill="#fdfdfd" font-size="65" font-family="Caveat-SemiBold, Caveat" font-weight="600"><tspan x="0" y="0">Rishi</tspan></text> */}
+          {/* </svg> */}
         </GatsbyLink>
         <div className="flex flex-row items-center justify-between">
           <div className="hidden md:flex flex-row">
