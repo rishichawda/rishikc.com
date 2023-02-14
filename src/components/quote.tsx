e5c0d6cf90f64b4f38211f420f5ce520b12e1d55
rishichawda <rishichawda@users.noreply.github.com>
2023-02-14 12:01:53 +0530

    minor refactor, clean up index page
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

---
+++ src/components/quote.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// TODO: Content is ready, just need to fetch and display.
+// Why haven't I done this yet?
+const RandomQuote = () => {
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto px-4 m:px-0 m:w-2/3 mb-28 justify-between">
+      <p className="text-xl antialiased italic text-center mb-10 leading-loose">“The happiness of those who want to be popular depends on others;
+        the happiness of those who seek pleasure fluctuates with moods outside their control;
+        but the happiness of the wise grows out of their own free acts.”</p>
+      <p className="text-xl antialiased text-center">Marcus Aurelius</p>
+    </div>
+  )
+}
+
+export default RandomQuote
