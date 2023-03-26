661929527835a9cfa6874059be93221f7eef7687
rishichawda <rishichawda@users.noreply.github.com>
2023-03-26 22:16:55 +0530

    initialise repo with latest deps
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { PageProps } from "gatsby"
+
+const NotFoundPage: React.FC<PageProps> = () => (
+  <section>
+    <h1>404: Not Found</h1>
+  </section>
+)
+
+export default NotFoundPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import type { PageProps } from "gatsby"
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main>
+      Main page
+    </main>
+  )
+}
+
+export default IndexPage
