b068eac03a857a441995f97b690ede8d64239a70
rishichawda <rishichawda@users.noreply.github.com>
2023-04-14 22:59:09 +0530

    move static query to hooks dir
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- src/components/repository/index.tsx
+++ src/components/repository/index.tsx
@@ -1,48 +1,11 @@
-import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
+import useGitRepositoryInfo from "../../hooks/use-git-repositores";
 import RepoCard from "./card";
 
 const Github = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      github {
-        viewer {
-          repositories(
-            orderBy: { field: STARGAZERS, direction: DESC }
-            first: 6
-            ownerAffiliations: OWNER
-          ) {
-            edges {
-              node {
-                id
-                name
-                url
-                descriptionHTML
-                stargazerCount
-                owner {
-                  login
-                }
-                languages(
-                  orderBy: { field: SIZE, direction: DESC }
-                  first: 10
-                ) {
-                  edges {
-                    node {
-                      color
-                      name
-                    }
-                    size
-                  }
-                  totalSize
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
+  const data = useGitRepositoryInfo();
+
   return (
     <div className="flex flex-col max-w-screen-2xl mx-auto mb-28 justify-between items-start github-repositories-preview">
       <h2 className="antialiased github-repositories-preview-header">
@@ -50,11 +13,9 @@ const Github = () => {
       </h2>
       <div className="mx-auto py-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-4 auto-cols-min gap-y-5 github-repositories-preview-list">
-          {data.github.viewer.repositories.edges.map(
-            (repository: Queries.GitHub_RepositoryEdge) => (
-              <RepoCard repository={repository.node} />
-            )
-          )}
+          {data.map((repository: Queries.GitHub_RepositoryEdge) => (
+            <RepoCard repository={repository.node} />
+          ))}
         </div>
       </div>
       <a
