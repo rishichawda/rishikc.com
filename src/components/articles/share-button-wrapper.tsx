f98136135bfa4619595c1c3fc9d728267c2f58e6
rishichawda <rishichawda@users.noreply.github.com>
2023-05-20 10:01:26 +0530

    fix pointer
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- src/components/articles/share-button-wrapper.tsx
+++ src/components/articles/share-button-wrapper.tsx
@@ -12,7 +12,7 @@ const ShareButtonWrapper = (props: Props) => {
         props.shareRef?.current?.click();
       }}
       tabIndex={0}
-      className="inline-flex items-center text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg shadow-sm hover:bg-gray-100 hover:text-brand-800 dark:bg-gray-800 dark:text-gray-400 dark:border-slate-700 dark:border dark:hover:text-white dark:hover:bg-gray-700 focus:bg-gray-200 dark:focus:bg-gray-700 focus:text-brand-800 dark:focus:text-white article-header-info-share-button"
+      className="inline-flex items-center text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg shadow-sm hover:bg-gray-100 hover:text-brand-800 dark:bg-gray-800 dark:text-gray-400 dark:border-slate-700 dark:border dark:hover:text-white dark:hover:bg-gray-700 focus:bg-gray-200 dark:focus:bg-gray-700 focus:text-brand-800 dark:focus:text-white hover:cursor-pointer article-header-info-share-button"
     >
       {props.children}
     </div>
