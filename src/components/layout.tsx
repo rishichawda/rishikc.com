b5d5d786f1d8f895801cd7f11ebf019f21fc3a44
rishichawda <rishichawda@users.noreply.github.com>
2023-05-24 20:31:58 +0530

    add bmc widget
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,6 +9,20 @@ const Layout: React.FC<LayoutProps> = ({ children, showScrollProgress }) => {
       <Navbar showScrollProgress={showScrollProgress} />
       {children}
       <Footer />
+      {/* Buy me a coffee */}
+      <script
+        defer
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="rishikc"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#BD5FFF"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
     </div>
   );
 };
