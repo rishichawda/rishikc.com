792d382c90f7acd691ab72594e9217b12c284d28
rishichawda <rishichawda@users.noreply.github.com>
2023-05-26 10:08:36 +0530

    i can't believe i forgot to do this
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -4,3 +4,6 @@ import './src/stylesheets/global.scss';
 import './src/stylesheets/scales/space.scss';
 import './src/stylesheets/scales/type.scss';
 
+// Reload the page when updates are found so that new changes are
+// visible without manual page refresh.
+exports.onServiceWorkerUpdateReady = () => window.location.reload();
