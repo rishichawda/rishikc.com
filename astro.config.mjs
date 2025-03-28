29c5a64cf3fd00f9323c09ed9359cf656dd65e03
rishichawda <rishichawda@users.noreply.github.com>
2025-03-28 15:23:44 +0530

    set up content collection for articles
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+import siteMetadata from './metadata';
+import { getEnv } from 'astro/env/runtime';
+import remarkToc from 'remark-toc';
+import rehypePresetMinify from 'rehype-preset-minify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'dracula' },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypePresetMinify],
+    remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: false,
+  })],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  site: siteMetadata.siteUrl,
+  trailingSlash: getEnv('NODE_ENV') == 'development' ? 'ignore' : 'always',
+  compressHTML: true,
+  security: {
+    checkOrigin: true
+  },
+});
---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders'; // Not available with legacy API
+
+const articles = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./content/articles" }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            subtitle: z.string().optional(),
+            description: z.string(),
+            hero_image: image(),
+            hero_image_alt: z.string().optional(),
+            hero_image_credit_text: z.string().optional(),
+            path: z.string(),
+            date: z.coerce.date(),
+            keywords: z.string(),
+            tags: z.array(z.string()),
+            category: z.enum(['General', 'Technology', 'Photography', 'Engineering and Development', 'Philosophy', 'Muse']).optional(),
+        }),
+});
+
+const quotes = defineCollection({
+    loader: file("content/reads/quotes.json", { parser: (text) => JSON.parse(text) })
+})
+
+export const collections = { articles, quotes };
